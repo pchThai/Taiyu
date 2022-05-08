e1a0ad9845085c913179cd8b9418728b760d53af
Phạm Châu Hồng Thái <88072404+pchThai@users.noreply.github.com>
2022-05-08 13:10:33 +0700

    Add Gorm and connect mysql

--- taiyu-back-end/main.go
+++ taiyu-back-end/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+	_, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/taiyu"), &gorm.Config{}) 
+	if err != nil{
+		panic("Could connect with DB")
+	}
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
