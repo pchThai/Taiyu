b64a1af2bea49e6901cb375815868a11d1d75ae7
Phạm Châu Hồng Thái <88072404+pchThai@users.noreply.github.com>
2022-10-25 22:46:28 +0700

    Add template

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,32 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Nav from './components/Nav';
+import Menu from './components/Menu';
 function App() {
   return (
     <div className="App">
-      <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Company name</a>
-      <div class="navbar-nav">
-        <div class="nav-item text-nowrap">
-          <a class="nav-link px-3" href="#">Sign out</a>
-        </div>
-      </div>
-    </header>
-
+      <Nav/>
+      <Menu/>
 <div class="container-fluid">
   <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3 sidebar-sticky">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">
-              <span data-feather="home" class="align-text-bottom"></span>
-              Dashboard
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+   
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <h2>Section title</h2>
